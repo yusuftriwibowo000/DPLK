eafefad3a28b386a33e25ae1371971abbc6e2cb0
Yusuf <yusuftriwibowo000@gmail.com>
2023-01-02 18:34:47 +0700

    Scripting DPLKINV002-001, DPLKINV002-002 and DPLKINV002-028 until DPLKINV002-030

--- DPLKINV002-002 -  Investasi - Reksadana - Entry Harga Pasar Upload Data/Report/Report/test_data_src.js
+++ DPLKINV002-002 -  Investasi - Reksadana - Entry Harga Pasar Upload Data/Report/Report/test_data_src.js
@@ -1,1 +1,1 @@
-﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\1427\\Documents\\BNI\\DPLK\\DPLKINV002-002 -  Investasi - Reksadana - Entry Data Pasar Upload Data\\Report',namePairs:[]}]};
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\1427\\Documents\\BNI\\DPLK\\DPLKINV002-002 -  Investasi - Reksadana - Entry Harga Pasar Upload Data\\Report',namePairs:[]}]};

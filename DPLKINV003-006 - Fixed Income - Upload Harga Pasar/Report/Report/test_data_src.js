7cbe8cf1a75bb78d4e90937ee142b71555588d77
Yusuf <yusuftriwibowo000@gmail.com>
2023-01-05 14:53:35 +0700

    Scripting DPLKINV003-004 and DPLKINV003-006 until DPLKINV003-008

--- DPLKINV003-006 - Fixed Income - Upload Harga Pasar/Report/Report/test_data_src.js
+++ DPLKINV003-006 - Fixed Income - Upload Harga Pasar/Report/Report/test_data_src.js
@@ -1,1 +1,1 @@
-﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\1427\\Documents\\BNI\\DPLK\\DPLKINV003-001 - Fixed Income - Upload Harga Pasar\\Report',namePairs:[]}]};
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\1427\\Documents\\BNI\\DPLK\\DPLKINV003-006 - Fixed Income - Upload Harga Pasar\\Report',namePairs:[]}]};
